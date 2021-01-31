f0c36277490797b2e820d83e1af78a2f22b43e9f
wangonya <kwangonya@gmail.com>
2021-01-31 15:33:57 +0300

    remove neovide, add postgres install

--- ubuntu_based_setup.sh
+++ ubuntu_based_setup.sh
@@ -25,23 +25,12 @@ fc-cache -fv
 
 echo
 echo ====================================
-echo installing neovide
+echo installing postgres
 echo ====================================
 echo
-sudo apt-get install -y \
-    gnupg ca-certificates \
-    gcc-multilib g++-multilib cmake libssl-dev pkg-config \
-    libfreetype6-dev libasound2-dev libexpat1-dev libxcb-composite0-dev \
-    libbz2-dev libsndio-dev freeglut3-dev libxmu-dev libxi-dev
-curl -sL "http://packages.lunarg.com/lunarg-signing-key-pub.asc" | sudo apt-key add -
-sudo curl -sLo "/etc/apt/sources.list.d/lunarg-vulkan-1.2.131-bionic.list" "http://packages.lunarg.com/vulkan/1.2.131/lunarg-vulkan-1.2.131-bionic.list"
-sudo apt-get update -y
-sudo apt-get install -y vulkan-sdk
-curl --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
-git clone "https://github.com/Kethku/neovide"
-cd neovide && ~/.cargo/bin/cargo build --release
-cp ./target/release/neovide ~/.neovide
-cd
+sudo apt install postgresql postgresql-contrib -y
+sudo -u postgres createuser --interactive
+createdb $USER
 
 echo
 echo ====================================
