1fb240af95533e93df78c305916637607c1f929b
kelvin <kelvin@jenga-agency.com>
2021-02-01 14:47:50 +0300

    remove github desktop install

--- ubuntu_based_setup.sh
+++ ubuntu_based_setup.sh
@@ -62,16 +62,6 @@ git clone https://github.com/wangonya/dotFiles.git --depth 1
 
 echo
 echo ====================================
-echo installing github desktop
-echo ====================================
-echo
-wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
-sudo apt-get update
-sudo apt install github-desktop
-
-echo
-echo ====================================
 echo installing oh-my-bash
 echo ====================================
 echo
